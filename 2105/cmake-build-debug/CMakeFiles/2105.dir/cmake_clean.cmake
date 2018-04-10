069cde409a53c7a02a23e68e7e2d877114da9e12
Carllll <nh222kim@gmail.com>
2018-04-10 11:12:37 +0900

    2105

---
+++ 2105/cmake-build-debug/CMakeFiles/2105.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2105.dir/main.cpp.o"
+  "2105.pdb"
+  "2105"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2105.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
